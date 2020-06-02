1c5d18a7aed469d16a918fa6e9b8919a3dd85dd2
Connor Edwards <38229097+cedws@users.noreply.github.com>
2020-06-02 20:19:54 +0100

    Small cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,46 @@
+#![feature(plugin)]
 #![feature(proc_macro_hygiene, decl_macro)]
 
+#[macro_use]
+extern crate rocket;
 extern crate flate2;
 extern crate image;
 extern crate reqwest;
-#[macro_use]
-extern crate rocket;
-
-use std::io::{Cursor, Write};
 
 use rocket::config::{Config, Environment};
+use rocket::http::hyper::header::{Connection, ContentEncoding, Encoding};
 use rocket::http::{RawStr, Status};
 use rocket::response::Response;
 
-use image::{imageops, FilterType, ImageBuffer, RgbImage};
-
 use flate2::write::GzEncoder;
 use flate2::Compression;
+use image::{imageops, FilterType, ImageBuffer};
 
-const MAX_OUTPUT_SIZE: f32 = 512.0;
+use std::io::{Cursor, Write};
 
-/// Resizes an input image to fit a maximum frame size whilst preserving the original image ratio.
+const MAX_OUTPUT_SIZE: u32 = 512;
+
+/// Resizes an input image to fill a maximum frame size whilst preserving the original image ratio.
 #[get("/<img>")]
 fn convert(img: &RawStr) -> Result<Response<'static>, Status> {
     let url = img.url_decode().map_err(|_| Status::BadRequest)?;
     let mut resp = reqwest::get(&url).map_err(|_| Status::BadRequest)?;
 
-    println!("Converting {}", &url);
-
     let mut buf = Vec::new();
     resp.copy_to(&mut buf)
         .expect("Failed to load response data");
 
-    let original = image::load_from_memory(buf.as_slice())
+    let original = image::load_from_memory(&buf)
         .map_err(|_| Status::BadRequest)?
         .to_rgb();
 
-    let width = original.width() as f32;
-    let height = original.height() as f32;
+    let (width, height) = (original.width(), original.height());
 
     // Find the optimum output size.
-    let size = (width.max(height)).min(MAX_OUTPUT_SIZE);
-    let scalar = (size / width).min(size / height);
+    let size = (width.max(height)).min(MAX_OUTPUT_SIZE) as f32;
+    let scalar = (size / width as f32).min(size / height as f32);
 
-    let (xscaled, yscaled) = (scalar * width, scalar * height);
+    let (xscaled, yscaled) = (scalar * width as f32, scalar * height as f32);
     let (xoverlay, yoverlay) = ((size - xscaled) / 2.0, (size - yscaled) / 2.0);
 
     let resized = imageops::resize(
@@ -52,21 +50,19 @@ fn convert(img: &RawStr) -> Result<Response<'static>, Status> {
         FilterType::Lanczos3,
     );
 
-    let mut rescaled = ImageBuffer::new(size as u32, size as u32) as RgbImage;
+    let mut rescaled = ImageBuffer::new(size as u32, size as u32);
     imageops::overlay(&mut rescaled, &resized, xoverlay as u32, yoverlay as u32);
 
-    // Assert that output image has a width and height of 512.
-    assert_eq!(rescaled.width(), size as u32);
-    assert_eq!(rescaled.height(), size as u32);
+    let mut enc = GzEncoder::new(Vec::new(), Compression::best());
+    enc.write_all(&rescaled.into_raw())
+        .map_err(|_| Status::InternalServerError)?;
 
-    // TODO: Don't assume the client supports GZIP compression.
-    let mut enc = GzEncoder::new(Vec::new(), Compression::default());
-    enc.write_all(&rescaled.into_raw()).unwrap();
+    let data = enc.finish().map_err(|_| Status::InternalServerError)?;
 
     Response::build()
-        .raw_header("Content-Encoding", "gzip")
-        .raw_header("Connection", "keep-alive")
-        .sized_body(Cursor::new(enc.finish().unwrap()))
+        .header(Connection::keep_alive())
+        .header(ContentEncoding(vec![Encoding::Gzip]))
+        .sized_body(Cursor::new(data))
         .ok()
 }
 
